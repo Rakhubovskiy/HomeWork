bad4929149358e259d9d1895c3aa8d224ff08970
Ilia <rakhubovskiy@gmail.com>
2023-01-29 22:09:42 +0300

    Task03 completed

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -2,3 +2,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.Clear();
+Console.Write("Insert the number = ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine(number + " - even number");
+}
+if (number % 2 == 1)
+{
+    Console.WriteLine(number + " - odd number");
+}
