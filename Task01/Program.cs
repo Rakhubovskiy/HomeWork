ff0a042c6ba0b135e1b9f95b1ff8cc74860bcedb
Ilia <rakhubovskiy@gmail.com>
2023-01-29 21:43:04 +0300

    Task01 completed

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,3 +2,21 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Insert the number A = ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Insert the number B = ");
+int numberB = int.Parse(Console.ReadLine());
+int max = numberA;
+int min = numberB;
+if (max > min)
+{
+    Console.WriteLine("Maximum number = " + max);
+    Console.WriteLine("Minimum number = " + min);
+}
+if (min > max)
+{
+    Console.WriteLine("Maximum number = " + min);
+    Console.WriteLine("Minimum number = " + max);
+}
