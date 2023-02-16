aa77c0f60a83edcf0b2837ce77f1cd09759ac158
Ilia <rakhubovskiy@gmail.com>
2023-02-16 21:52:39 +0300

    Task15 completed

---
+++ Task15/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов на нечётных позициях: {SumArray(array)}");
+
+void FillArray(int[] fillArray)
+{
+    int length = fillArray.Length;
+    for (int index = 0; index < length; index++)
+    {
+        fillArray[index] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] printArray)
+{
+    int length = printArray.Length;
+    for (int index = 0; index < length; index++)
+    {
+        Console.Write($"{printArray[index]} ");
+    }
+}
+
+int SumArray(int[] sumArray)
+{
+    int sum = 0;
+    int length = sumArray.Length;
+    for (int index = 0; index <= length; index++)
+    {
+        if (index % 2 > 0)
+        {
+            sum = sum + sumArray[index];
+        }
+    }
+    return sum;
+}
