c0c3d8419f4444423c7f15fb55feb96e8bd186e4
Ilia <rakhubovskiy@gmail.com>
2023-02-18 18:09:44 +0300

    Task17 completed

---
+++ Task17/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+Console.WriteLine(string.Join(" ", array));
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Чисел больше 0: {count}");
