9357a2686a54bbd69dbb8c45fd11b34a425a249b
Ilia <rakhubovskiy@gmail.com>
2023-02-26 21:53:29 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+Console.Write("Введите количество строк  массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцы  массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+SortArrayColumns(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArrayColumns(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 1; j < inArray.GetLength(1); j++)
+        {
+            for (int o = 0; o < inArray.GetLength(0); o++)
+            {
+                for (int k = 0; k < inArray.GetLength(1); k++)
+                {
+                    if (inArray[i, j] > inArray[o, k])
+                    {
+                        int temp = inArray[i, j];
+                        inArray[i, j] = inArray[o, k];
+                        inArray[o, k] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
